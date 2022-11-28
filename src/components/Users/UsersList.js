3cfc3461b2658241b4f89d9a2d2163d2782f1e3d
Amanullah Aman <amancse.aiub@gmail.com>
2022-11-28 12:03:57 +0600

    title

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -6,6 +6,11 @@ import classes from "./UsersList.module.css";
 const UsersList = (props) => {
   return (
     <Card className={classes.users}>
+      <header>
+        <h2>List of users</h2>
+      </header>
+      <hr />
+
       <ul>
         {props.users.map((user) => (
           <li key={user.id}>
